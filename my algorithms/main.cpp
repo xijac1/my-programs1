f0d6e3addaf783045e4dffbf169f4ddf4ae7a772
Julio Amaya <julioamaya127@gmail.com>
2024-09-26 21:12:24 -0500

    Initial Commit

---
+++ my algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my algorithms
+//
+//  Created by Julio Amaya on 9/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
